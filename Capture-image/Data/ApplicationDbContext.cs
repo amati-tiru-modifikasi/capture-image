8bb59c60b054e38e23691c85019e0ba9a4274d6c
mdestafadilah <desta.08b@gmail.com>
2024-03-18 14:50:22 +0700

    add-migration Initial

---
+++ Capture-image/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Capture_image.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Capture_image.Models.ImageModel> Images { get; set; }
+    }
+}
---
+++ Capture-image/Models/ImageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Capture_image.Models
+{
+    public class ImageModel
+    {
+        public int Id {  get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
