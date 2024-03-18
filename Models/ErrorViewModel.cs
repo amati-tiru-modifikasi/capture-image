29baa952e38b643b41925ed8e207f28f4a65a148
mdestafadilah <desta.08b@gmail.com>
2024-03-18 13:52:43 +0700

    rearranged

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CaptureImage.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
